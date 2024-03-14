66385c7795e796bb14e7f208763dc537cba72d22
adamd <adam.d.hinton@gmail.com>
2024-03-14 11:35:05 -0400

    my first BinaryOperator

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -2,6 +2,7 @@ package dev.lpa;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BinaryOperator;
 
 public class Main {
     public static void main(String[] args) {
@@ -38,12 +39,17 @@ public class Main {
         });
 
 
-        int result = calculator((a, b) -> a + b, 5, 2);
+        System.out.println("-------");
+
+
+        int result = calculator((var a, var b) -> {return a + b;}, 5, 2);
+        var result2 = calculator((a, b) -> a / b, 10.0, 2.5);
+        var result3 = calculator((a, b) -> a.toUpperCase() + " " + b.toUpperCase(), "Ralph", "Kramden");
 
     }
 
-    public static <T> T calculator(Operation<T> function, T value1, T value2){
-        T result = function.operate(value1, value2);
+    public static <T> T calculator(BinaryOperator<T> function, T value1, T value2){
+        T result = function.apply(value1, value2);
         System.out.println("Result: " + result);
         return result;
     }
