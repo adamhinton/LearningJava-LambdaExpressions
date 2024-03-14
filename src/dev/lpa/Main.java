8143b5a60ad4263983a34e9326c125dba31ecb83
adamd <adam.d.hinton@gmail.com>
2024-03-14 11:49:43 -0400

    Different ways to pass in lambdas

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,7 +1,9 @@
 package dev.lpa;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
 
 public class Main {
@@ -46,12 +48,35 @@ public class Main {
         var result2 = calculator((a, b) -> a / b, 10.0, 2.5);
         var result3 = calculator((a, b) -> a.toUpperCase() + " " + b.toUpperCase(), "Ralph", "Kramden");
 
+        var coords = Arrays.asList(
+          new double[] {47.2160, -95.2348},
+          new double[] {29.1566, -89.2495},
+          new double[] {35.1566, -90.0659}
+        );
+
+        coords.forEach(s -> System.out.println(Arrays.toString(s)));
+
+        BiConsumer<Double, Double> p1 =(lat, lng) ->
+            System.out.printf("[lat:%.3f long:%.3f]%n", lat, lng);
+
+        var firstPoint = coords.get(0);
+        processPoint(firstPoint[0], firstPoint[1], p1);
+
+        System.out.println("-------");
+
+        coords.forEach(s -> processPoint(s[0], s[1], p1));
+        coords.forEach(s -> processPoint(s[0], s[1], (lat, lng) ->
+                System.out.printf("[lat:%.3f long:%.3f]%n", lat, lng)));
     }
 
     public static <T> T calculator(BinaryOperator<T> function, T value1, T value2){
         T result = function.apply(value1, value2);
         System.out.println("Result: " + result);
         return result;
+    }
+
+    public static <T> void processPoint (T t1, T t2, BiConsumer<T, T> consumer){
+        consumer.accept(t1, t2);
     }
 
 }
