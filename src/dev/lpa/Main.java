5a4227026905a1098c294fc660771796febae1b8
adamd <adam.d.hinton@gmail.com>
2024-03-14 00:31:00 -0400

    call my functional interface

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -38,5 +38,14 @@ public class Main {
         });
 
 
+        int result = calculator((a, b) -> a + b, 5, 2);
+
     }
+
+    public static <T> T calculator(Operation<T> function, T value1, T value2){
+        T result = function.operate(value1, value2);
+        System.out.println("Result: " + result);
+        return result;
+    }
+
 }
